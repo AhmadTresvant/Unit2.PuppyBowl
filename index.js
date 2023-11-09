fcc144bdb41bf9d26bc0b6e0b65fe7484730bad3
Ahmad Ali Tresvant <ahmadtresvant@Alis-M1-Pro.local>
2023-11-09 14:36:59 -0500

    all li are now rendering info about each pup on the page

--- index.js
+++ index.js
@@ -19,10 +19,34 @@ const init = async () => {
   await getAllPlayers();
 }
 
+const getPuppyDetails = async (id) => {
+  const response = await fetch(`${apiLink}/players/${id}`);
+  const responseJson = await response.json();
+  const puppyDetails = responseJson.data;
+  renderDetails(puppyDetails);
+}
+
+const renderDetails = (puppyDetails) => {
+const html = `<h2>${puppyDetails.player.name}</h2
+
+<img src="${puppyDetails.player.imageUrl}"/>
+<p>ID:${puppyDetails.player.id}</p>
+<p>BREED:${puppyDetails.player.breed}</p>
+<p>STATUS:${puppyDetails.player.status}</p>
+<p>CREATED AT:${puppyDetails.player.createdAt}</p>
+<p>UPDATED AT:${puppyDetails.player.updatedAt}</p>
+<p>TEAM ID:${puppyDetails.player.teamId}</p>
+<p>COHORT ID:${puppyDetails.player.cohortId}</p>
+`
+
+main.innerHTML = html;
+}
+
 const renderAllPlayers = (state) => {
   const puppiesInLi = state.allPuppies.map((eachPup) => {
     return `<li id="${eachPup.id}">${eachPup.name}</li>`;
   });
+  
   const puppyRosterUl = document.createElement(`ul`);
   puppyRosterUl.innerHTML = puppiesInLi.join('');
   main.appendChild(puppyRosterUl);
@@ -30,7 +54,7 @@ const renderAllPlayers = (state) => {
   const eachPupOnRoster = document.querySelectorAll('li');
   eachPupOnRoster.forEach((pup) => {
     pup.addEventListener('click', (event) => {
-      console.log(event.target);
+      getPuppyDetails(event.target.id);
     });
   });
 }
